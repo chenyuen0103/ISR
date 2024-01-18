9807a75ed1203c469ccb56ba523a08ee32538156
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-01-18 17:19:30 -0600

    add breakpoint

--- real_datasets/utils/loss_utils.py
+++ real_datasets/utils/loss_utils.py
@@ -179,7 +179,7 @@ class LossComputer:
             # # Gradient and Hessian Computation assumes negative log loss
             # # grads = self.gradient(model, x[idx], y[idx])
             # get grads, hessian of loss with respect to parameters, and those to be backwarded later
-            breakpoint()
+            # breakpoint()
             loss.backward(retain_graph=True)
             # grads = torch.autograd.grad(loss, model.parameters(), create_graph=True)
             grads = self.gradient(model, x[idx], y[idx])
