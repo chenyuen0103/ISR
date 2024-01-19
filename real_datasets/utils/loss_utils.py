a63ba304193a65f16725e39a86a0d5424fc9d553
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-01-19 12:11:40 -0600

    back to resnet

--- real_datasets/utils/loss_utils.py
+++ real_datasets/utils/loss_utils.py
@@ -110,8 +110,7 @@ class LossComputer:
 
         # First order gradients
         # grads = torch.autograd.grad(loss, model.linear.weight, create_graph=True)[0]
-        grads = torch.autograd.grad(loss, [param for param in model.parameters() if param.requires_grad],
-                                    create_graph=True, allow_unused=True)
+        grads = torch.autograd.grad(loss, [param for param in model.parameters() if param.requires_grad], create_graph=True, allow_unused=True)
 
         # Flatten all gradients to a single vector (for a full Hessian)
         grad_vector = torch.cat([grad.view(-1) for grad in grads  if grad is not None])
@@ -120,9 +119,7 @@ class LossComputer:
         hessian = []
         for i in range(len(grad_vector)):
             # Compute gradients with respect to each element of the gradient vector
-            row_grads = torch.autograd.grad(grad_vector[i],
-                                            [param for param in model.parameters() if param.requires_grad],
-                                            create_graph=True, retain_graph=True, allow_unused=True)
+            row_grads = torch.autograd.grad(grad_vector[i], [param for param in model.parameters() if param.requires_grad], create_graph=True, retain_graph=True, allow_unused=True)
 
             # Flatten and append to the Hessian
             row = torch.cat([g.view(-1) for g in row_grads])
