12aabb2a3a005c2a6094920340350598b51e84c4
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-01-18 21:06:20 -0600

    add a empty_cache()

--- real_datasets/launch_train.py
+++ real_datasets/launch_train.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 from configs import get_train_command
 import pdb
 
-gpu_idx = 0,1,2,3  # could be None if you want to use cpu
+# gpu_idx = 0,1,2,3  # could be None if you want to use cpu
 
 algos = ['ERM','reweight','groupDRO']
 # dataset = 'MultiNLI'  # could be 'CUB' (i.e., Waterbirds), 'CelebA' or 'MultiNLI'
@@ -12,10 +12,31 @@ dataset = 'CUB'
 # can add some suffix to the algo name to flag the version,
 # e.g., with algo_suffix = "-my_version", the algo name becomes "ERM-my_version"
 algo_suffix = ""
+# Assuming seeds, algos, dataset, and get_train_command are defined
 
+gpu_count = 4  # Number of GPUs available
+gpu_idx = 0    # Start with GPU 0
 seeds = range(10)
 for seed, algo in tqdm(list(product(seeds, algos)), desc='Experiments'):
+    # Generate the command
     command = get_train_command(dataset=dataset, algo=algo, gpu_idx=gpu_idx, seed=seed,
-                      save_best=True, save_last=True)
+                                save_best=True, save_last=True)
     print('Command:', command)
-    os.system(command)
+
+    # Run the command in the background
+    os.system(command + " &")
+
+    # Rotate the GPU index
+    gpu_idx = (gpu_idx + 1) % gpu_count
+
+    # Optional: Introduce a slight delay if needed
+    # time.sleep(1)
+
+# Wait for all background processes to finish
+os.system("wait")
+
+# for seed, algo in tqdm(list(product(seeds, algos)), desc='Experiments'):
+#     command = get_train_command(dataset=dataset, algo=algo, gpu_idx=gpu_idx, seed=seed,
+#                       save_best=True, save_last=True)
+#     print('Command:', command)
+#     os.system(command)
