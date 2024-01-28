e4a42d610222df4c713d3415a31ba78f95c792dd
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-01-28 16:12:34 -0600

    add breakpoint

--- real_datasets/train_hessian.py
+++ real_datasets/train_hessian.py
@@ -54,8 +54,9 @@ def run_epoch(epoch, model, optimizer, loader, loss_computer, logger, csv_logger
                 )[1]  # [1] returns logits
             elif args.model == 'clip':
                 # Reshape x to [batch_size, channels, height, width]
-                x = x.view(x.size(0), 3, 224, 224) # Replace 224, 224 with the correct dimensions if different
-                # outputs = model(x)
+                x = x.view(x.size(0), 3, 224, 224)
+                breakpoint()# Replace 224, 224 with the correct dimensions if different
+                outputs = model(x)
             # else:
             #     outputs = model(x)
 
