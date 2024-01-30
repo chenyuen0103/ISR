6699b211088673d15e274e8061a3023ec43d2abd
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-01-30 16:51:59 -0600

    log hessian and gradient norm

--- real_datasets/utils/loss_utils.py
+++ real_datasets/utils/loss_utils.py
@@ -259,10 +259,10 @@ class LossComputer:
         # Compute average gradient and hessian
         # avg_gradient = [torch.mean(torch.stack([grads[i] for grads in env_gradients]), dim=0) for i in
         #                 range(len(env_gradients[0]))]
-        weight_gradients = [g[0] for g in env_gradients if g.nelement() > 0]
+        weight_gradients = [g[0] for g in env_gradients if g.dim() > 1]
         breakpoint()
         avg_gradient = torch.mean(torch.stack(weight_gradients), dim=0)
-        filtered = [h for h in env_hessians if h.nelement() > 0]
+        filtered = [h for h in env_hessians if h.dim() > 2]
         # avg_gradient = torch.mean(torch.stack(env_gradients), dim=0)
         avg_hessian = torch.mean(torch.stack(filtered), dim=0)
 
