842760527d76011b989e4c016fd0901441f79798
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-01-31 22:10:39 -0600

    debug print

--- real_datasets/train_hessian.py
+++ real_datasets/train_hessian.py
@@ -100,7 +100,7 @@ def run_epoch(epoch, model,clf, optimizer, loader, loss_computer, logger, csv_lo
                 # else:
                 #     outputs = model(x)
                 if args.hessian_align:
-                    print('Hessian Align:', args.hessian_align)
+                    # print('Hessian Align:', args.hessian_align)
                     loss_main, _, _, _ = loss_computer.exact_hessian_loss(clf, outputs, y, g)
                 else:
                     loss_main = loss_computer.loss(outputs, y, g, is_training)
