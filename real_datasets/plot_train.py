1b8855fa8591618f3e92061176981c3478032031
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-02-04 17:36:05 -0600

    check batch

--- real_datasets/plot_train.py
+++ real_datasets/plot_train.py
@@ -15,30 +15,30 @@ test_df =  pd.read_csv(f'../logs/{dataset}/{model}/{algo}/s{seed}/test.csv')
 worst_case_train_acc = train_df.groupby('epoch')['avg_acc'].min().reset_index()
 worst_case_val_acc = val_df.groupby('epoch')['avg_acc'].min().reset_index()
 
-plt.figure(figsize=(14, 7))
 
-# Plot for Training and Validation Loss
-plt.subplot(1, 2, 1)
-plt.plot(train_df['epoch'], train_df['avg_actual_loss'], label='Training Loss')
-plt.plot(val_df['epoch'], val_df['avg_actual_loss'], label='Validation Loss')
+# Plot Training and Validation Loss
+plt.figure(figsize=(7, 5))
+plt.plot(train_df['epoch'], train_df['avg_actual_loss'], label='Training')
+plt.plot(val_df['epoch'], val_df['avg_actual_loss'], label='Validation')
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
-plt.title('Training and Validation Loss')
+plt.title('Average Loss (Cross Entropy)')
 plt.legend()
 plt.xlim(0, 300)
-plt.savefig(f'../logs/{dataset}/{model}/{algo}/s{seed}/train_val_loss.pdf')  # Save the figure
+plt.savefig(f'../logs/{dataset}/{model}/{algo}/s{seed}/{dataset}_loss.pdf')
+plt.show()
 plt.close()
 
-# Plot for Worst-case Training and Validation Accuracy
-plt.subplot(1, 2, 2)
-plt.plot(worst_case_train_acc['epoch'], worst_case_train_acc['avg_acc'], label='Worst-case Training Accuracy', linestyle='-')
-plt.plot(worst_case_val_acc['epoch'], worst_case_val_acc['avg_acc'], label='Worst-case Validation Accuracy', linestyle='-')
+
+# Plot Worst-case Training and Validation Accuracy
+plt.figure(figsize=(7, 5))
+plt.plot(worst_case_train_acc['epoch'], worst_case_train_acc['avg_acc'], label='Training', linestyle='-')
+plt.plot(worst_case_val_acc['epoch'], worst_case_val_acc['avg_acc'], label='Validation', linestyle='-')
 plt.xlabel('Epoch')
-plt.ylabel('Worst-case Accuracy')
-plt.title('Worst-case Training and Validation Accuracy')
+plt.ylabel('Accuracy')
+plt.title('Worst-case Accuracy')
 plt.legend()
 plt.xlim(0, 300)
-plt.savefig(f'../logs/{dataset}/{model}/{algo}/s{seed}/train_val_acc.pdf')  # Save the figure
-plt.close()
-plt.tight_layout()
+plt.savefig(f'../logs/{dataset}/{model}/{algo}/s{seed}/{dataset}_acc.pdf')
 plt.show()
+plt.close()
