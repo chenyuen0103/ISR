c4aa3ae69798bf67928e68ef5a9d4dcbbef3b520
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-02-07 16:00:41 -0600

    not resuming s0

--- real_datasets/train_cub.sh
+++ real_datasets/train_cub.sh
@@ -6,11 +6,5 @@ python launch_train.py --dataset CUB --gpu_idx 0 --scheduler
 python launch_train.py --dataset CUB --gpu_idx 1 --scheduler
 python launch_train.py --dataset CUB --gpu_idx 2 --scheduler
 python launch_train.py --dataset CUB --gpu_idx 3 --scheduler
-python launch_train.py --dataset CUB --gpu_idx 0 --scheduler
-python launch_train.py --dataset CUB --gpu_idx 1 --scheduler
-python launch_train.py --dataset CUB --gpu_idx 2 --scheduler
-python launch_train.py --dataset CUB --gpu_idx 3 --scheduler
-python launch_train.py --dataset CUB --gpu_idx 0 --scheduler
-python launch_train.py --dataset CUB --gpu_idx 1 --scheduler
-python launch_train.py --dataset CUB --gpu_idx 2 --scheduler
-python launch_train.py --dataset CUB --gpu_idx 3 --scheduler
+
+git restore train_cub.sh
