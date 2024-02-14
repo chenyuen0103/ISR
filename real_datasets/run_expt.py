f2e6479692e8019fcdc935934cdd0edcbbd670ac
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-02-14 15:33:14 -0600

    resume if log_dir already exists

--- real_datasets/run_expt.py
+++ real_datasets/run_expt.py
@@ -96,7 +96,9 @@ def main():
     args.log_dir = os.path.join(args.log_dir, args.dataset, args.model, algo + args.algo_suffix, f's{args.seed}',
                                 f'grad_alpha_{grad_alpha_formatted}_hess_beta_{hess_beta_formatted}')
 
-    if os.path.exists(args.log_dir) and args.resume:
+    breakpoint()
+    # if os.path.exists(args.log_dir) and args.resume:
+    if os.path.exists(args.log_dir):
         resume = True
         mode = 'a'
     else:
