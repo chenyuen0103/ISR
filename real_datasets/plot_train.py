f8d694cf68c0908ce71b91e0d005dbdd5ad74ef1
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-02-15 20:34:39 -0600

    fix last_clf

--- real_datasets/plot_train.py
+++ real_datasets/plot_train.py
@@ -10,7 +10,7 @@ algo = 'HessianERM'
 seed = 0
 
 grad_alpha = 1e-6
-hess_beta = 1e-6
+hess_beta = 1e-4
 
 grad_alpha_formatted = "{:.1e}".format(grad_alpha).replace('.0e', 'e')
 hess_beta_formatted = "{:.1e}".format(hess_beta).replace('.0e', 'e')
