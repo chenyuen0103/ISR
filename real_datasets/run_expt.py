c520f1bd7ba685c54bde81b8e3a2dce5154fd354
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-02-18 15:23:14 -0600

    fix erm

--- real_datasets/run_expt.py
+++ real_datasets/run_expt.py
@@ -93,12 +93,12 @@ def main():
     hess_beta_formatted = "{:.1e}".format(args.hess_beta).replace('.0e', 'e')
 
     # args.log_dir = os.path.join(args.log_dir, args.dataset, args.model, algo + args.algo_suffix, f's{args.seed}', f'grad_alpha_{args.grad_alpha}_hess_beta_{args.hess_beta}')
-    args.log_dir = os.path.join(args.log_dir, args.dataset, args.model, algo + args.algo_suffix, f's{args.seed}',
-                                    f"grad_alpha_{grad_alpha_formatted}_hess_beta_{hess_beta_formatted}{'_no_scheduler' if not args.scheduler else ''}")
-
     if args.dataset == 'MultiNLI':
         args.log_dir = os.path.join(args.log_dir, args.dataset, args.model, algo + args.algo_suffix, f's{args.seed}',
                                     f"grad_alpha_{grad_alpha_formatted}_hess_beta_{hess_beta_formatted}")
+    else:
+        args.log_dir = os.path.join(args.log_dir, args.dataset, args.model, algo + args.algo_suffix, f's{args.seed}',
+                                    f"grad_alpha_{grad_alpha_formatted}_hess_beta_{hess_beta_formatted}{'_no_scheduler' if not args.scheduler else ''}")
 
     print(f'Logging to {args.log_dir}')
     breakpoint()
