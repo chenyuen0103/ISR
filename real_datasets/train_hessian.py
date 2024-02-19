026cda66b9755e639cfa553e3ddec6814c7d374c
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-02-19 09:32:06 -0600

    add siglip-512

--- real_datasets/train_hessian.py
+++ real_datasets/train_hessian.py
@@ -167,7 +167,8 @@ def train(model,clf, criterion, dataset,
     num_classes = 2
     if args.model == 'clip512':
         dummy_input = torch.randn(1, 3, 512, 512).cuda()
-    if args.model == 'clip':
+        encoder = model.encode_image
+    elif args.model == 'clip':
         dummy_input = torch.randn(1, 3, 224, 224).cuda()
         encoder = model.encode_image
         with torch.no_grad():
