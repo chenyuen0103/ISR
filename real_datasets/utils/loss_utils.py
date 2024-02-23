f66dfe13166fedc55028c59f6bf05bf03f1e7a10
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-02-23 15:50:03 -0600

    modify ISR

--- real_datasets/utils/loss_utils.py
+++ real_datasets/utils/loss_utils.py
@@ -281,6 +281,7 @@ class LossComputer:
             idx = (envs_indices == env_idx).nonzero().squeeze()
             if idx.numel() == 0:
                 continue
+            breakpoint()
             num_samples = len(idx)
             yhat = logits[idx]
             loss = self.criterion2(yhat.squeeze(), y[idx].long())
