34b123e686b18cf029432ac6342aba082f7295a9
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-03-04 16:04:37 -0600

    rmove to cpu

--- real_datasets/eval.py
+++ real_datasets/eval.py
@@ -214,7 +214,7 @@ if __name__ == '__main__':
     os.environ["CUDA_VISIBLE_DEVICES"] = str(args.cuda)
     # loop over alpha and beta values in [0, 1e-7, 1e-6,1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 0]
     alpha_list = [1e-7, 1e-6, 1e-5, 1e-3, 1e-2, 1e-1, 0, 1e-4][::-1]
-    beta_list = [1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 0][::-1]
+    beta_list = [1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 0, 1e-2, 1e-1][::-1]
     seed_list = [1]
     for alpha, beta, seed in product(alpha_list, beta_list, seed_list):
         print(f"Running for alpha = {alpha}, beta = {beta}, seed = {seed} in {args.dataset}")
