0b542445855911fd42e1d6cc4897b8af468659de
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-04-08 09:43:37 -0500

    parse multinli

--- real_datasets/parse_features_clip.py
+++ real_datasets/parse_features_clip.py
@@ -103,8 +103,7 @@ check_args(args)
 # model = model.eval()
 if args.dataset == "MultiNLI":
     model,  _, preprocess  = open_clip.create_model_and_transforms('ViT-B-32',
-                                                                   pretrained='laion2b_s34b_b79k',
-                                                                   num_classes=0)
+                                                                   pretrained='laion2b_s34b_b79k')
     tokenizer = open_clip.get_tokenizer('ViT-B-32')
     model = model.eval()
     model = model.to('cuda')
