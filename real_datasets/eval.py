2c0642e5ad31bc070b880810535ddccbc474810e
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-04-23 21:36:28 -0500

    test celeba

--- real_datasets/eval.py
+++ real_datasets/eval.py
@@ -322,8 +322,8 @@ if __name__ == '__main__':
         args.model_select = 'init'
         penalty_anneal_iters_list = np.linspace(0, 1400, 5)[1:]
     if args.dataset == 'CelebA':
-        alpha_list = [0.01, 0, 1000, 5000]
-        beta_list = [0.01, 0, 1000, 5000]
+        alpha_list = [0.01, 0, 1000, 5000][::-1]
+        beta_list = [0.01, 0, 1000, 5000][::-1]
         args.max_iter = 50
         args.save_dir = './logs/ISR_Hessian_results_ViT-B_scaled'
         args.root_dir = './inv-feature-ViT-B/logs'
