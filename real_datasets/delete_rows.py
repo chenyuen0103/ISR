b49390f314d01439e05c5974b04bdab8c1737c83
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-05-05 23:11:52 -0500

    search nli

--- real_datasets/delete_rows.py
+++ real_datasets/delete_rows.py
@@ -4,9 +4,11 @@ import glob
 
 
 patterns = ['CelebA_results_s*_hessian_exact.csv','CUB_results_s*_hessian_exact.csv','MultiNLI_results_s*_hessian_exact.csv']
-data_dir = '../logs/ISR_Hessian_results_new'
+data_dir = './logs/ISR_Hessian_results_new'
 print(os.getcwd())
 for pattern in patterns:
     files = glob.glob(os.path.join(data_dir, pattern))
     for file in files:
-        print(file)
+        df = pd.read_csv(file)
+        df = df[df['penalty_anneal_iters'] == 0]
+        df.to_csv(file, index=False)
